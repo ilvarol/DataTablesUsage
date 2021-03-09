5a491f16c99e4947e73cea687bea9b703a84f3ec
ilyas.varol <ilyas.varol@outlook.com.tr>
2021-03-09 18:54:10 +0300

    Add project files.

---
+++ DataTablesUsage/ApiController/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using DataTablesUsage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataTablesUsage.Pages
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DataTablesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Test()
+        {
+            var jsonString = System.IO.File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}/Mock Data/MOCK_DATA.json");
+            var mockDataList = JsonSerializer.Deserialize<List<MockData>>(jsonString);
+
+            var start = int.Parse(Request.Form["start"].ToString());
+            var length = int.Parse(Request.Form["length"].ToString());
+            var search = Request.Form["search[value]"].ToString();
+
+            mockDataList = mockDataList.Skip(start).Take(length).ToList();
+
+            return Ok(mockDataList);
+        }
+    }
+}
---
+++ DataTablesUsage/Models/MockData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataTablesUsage.Models
+{
+    public class MockData
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
